03e8c23079ca6a42fda3f6f871fdec5023afe995
ho-o <funnybarney@gmail.com>
2021-07-26 19:12:16 +0300

    Initial commit

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+//theme set up
+// marked up in HTML5
+add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+// automatically generate feed links
+add_theme_support( 'automatic-feed-links' );
+//setting to use eye catchhing images
+add_theme_support( 'post-thumbnails' );
