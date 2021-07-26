f7946f1ff7a183be84f24175092d9ff55b2ed605
ho-o <funnybarney@gmail.com>
2021-07-26 21:24:30 +0300

    changed header.php

--- header.php
+++ header.php
@@ -2,5 +2,14 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-
+  <meta name=”viewport” content=”width=device-width,initial-scale=1.0″>
+  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+  <?php wp_head()?>
 </head>
+
+<body <?php bpdy_class(); ?>>
+  <header>
+      <div class="header-inner">
+
+      </div>
+  </header>
